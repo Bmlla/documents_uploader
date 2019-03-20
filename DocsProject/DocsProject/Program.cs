4f0152fdd2a5fe3ef10a0214f430372389fa8e7e
Bruno Muller <bbrunomlla@gmail.com>
2019-03-20 10:02:58 -0300

    deploy heroku

--- DocsProject/DocsProject/Program.cs
+++ DocsProject/DocsProject/Program.cs
@@ -14,6 +14,16 @@ namespace DocsProject
     {
         public static void Main(string[] args)
         {
+            var config = new ConfigurationBuilder().AddCommandLine(args).Build();
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseConfiguration(config)
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
             CreateWebHostBuilder(args).Build().Run();
         }
 
